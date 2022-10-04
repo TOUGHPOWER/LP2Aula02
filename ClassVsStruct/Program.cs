21400921364366b1e771a49896012ec9a279b45b
Pedroosorio97 <pedromrnosorio97@gmail.com>
2022-10-04 10:37:27 +0100

    Update DoubleHealth

--- ClassVsStruct/Program.cs
+++ ClassVsStruct/Program.cs
@@ -10,14 +10,15 @@ namespace ClassVsStruct
             Player player2 = player1;
             Console.WriteLine("Player1:" + player1.Health + " " + player1.Armor);
             Console.WriteLine("Player2:" + player2.Health + " " + player2.Armor);
-            player1.Health = DoubleHealth(player1);
+            DoubleHealth(ref player1);
             Console.WriteLine("Player1:" + player1.Health + " " + player1.Armor);
             Console.WriteLine("Player2:" + player2.Health + " " + player2.Armor);
         }
 
-        static int DoubleHealth(Player player)
+        private static void DoubleHealth(ref Player player)
         {
-            return player.Health * 2;
+            player.Health *= 2;
+            
         }
     }
 }
